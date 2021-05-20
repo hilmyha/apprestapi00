76b2ffd6efa97dd35856bbf56a23cc4368b2d199
Haidar <68181532+hilmyha@users.noreply.github.com>
2021-05-20 22:28:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 // pharse app json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+// call route
+var routes = require('./routes');
+routes(app);
 
 app.listen(3000, () => {
     console.log(`Server started on port`);
