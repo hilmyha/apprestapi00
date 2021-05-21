c96ed977331f1878fbef8ecf1b408ebecc30a9fb
Haidar <68181532+hilmyha@users.noreply.github.com>
2021-05-21 10:14:19 +0700

    update controller & route

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Rest API running",res)
 };
+
+// show databases
+exports.tampildatamhs = function (row, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
